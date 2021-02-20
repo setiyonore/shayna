76d04b2029e01548b39207c8b3e2113eaf3d8e70
setiyonore <setiyono.ressly@gmail.com>
2021-02-20 20:17:48 +0700

    change class active and product big img on click

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,7 +2,7 @@
   <div class="product">
     <HeaderShayna/>
     <!-- Breadcrumb Section Begin -->
-    <div class="breacrumb-section">
+    <div class="breacrumb-section text-left">
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
@@ -24,30 +24,30 @@
             <div class="row">
               <div class="col-lg-6">
                 <div class="product-pic-zoom">
-                  <img class="product-big-img" src="img/mickey1.jpg" alt="" />
+                  <img class="product-big-img" :src="gambar_default" alt="" />
                 </div>
                 <div class="product-thumbs">
-                  <carousel :dots="false" :autoplay="true" :loop="true" :nav="false" class="product-thumbs-track ps-slider">
-                    <div class="pt active" data-imgbigurl="img/mickey1.jpg">
+                  <carousel :dots="false" :nav="false" class="product-thumbs-track ps-slider">
+                    <div class="pt" @click="changeImage(thumbs[0])" :class="thumbs[0] == gambar_default ? 'active' : '' ">
                       <img src="img/mickey1.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey2.jpg">
+                    <div class="pt" @click="changeImage(thumbs[1])" :class="thumbs[1] == gambar_default ? 'active' : '' ">
                       <img src="img/mickey2.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey3.jpg">
+                    <div class="pt" @click="changeImage(thumbs[2])" :class="thumbs[2] == gambar_default ? 'active' : ''">
                       <img src="img/mickey3.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey4.jpg">
+                    <div class="pt" @click="changeImage(thumbs[3])" :class="thumbs[3] == gambar_default ? 'active' : ''">
                       <img src="img/mickey4.jpg" alt="" />
                     </div>
                   </carousel>
                 </div>
               </div>
               <div class="col-lg-6">
-                <div class="product-details">
+                <div class="product-details text-left">
                   <div class="pd-title">
                     <span>oranges</span>
                     <h3>Pure Pineapple</h3>
@@ -92,7 +92,24 @@ export default {
     FooterShayna,
     carousel,
 
-  }
+  },
+  data(){
+    return {
+      gambar_default:"img/mickey1.jpg",
+        thumbs:[
+            "img/mickey1.jpg",
+            "img/mickey2.jpg",
+            "img/mickey3.jpg",
+            "img/mickey4.jpg",
+
+        ],
+    }
+  },
+  methods: {
+    changeImage(urlImage) {
+      this.gambar_default = urlImage;
+    }
+  },
 }
 </script>
 <style scoped>
