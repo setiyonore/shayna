51b1c2fb79a155f69db6b1bf6c1a575bde3aab4a
stynore <setiyono.ressly@gmail.com>
2021-03-25 21:24:22 +0700

    integrasi product details to api

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -26,22 +26,11 @@
                 <div class="product-pic-zoom">
                   <img class="product-big-img" :src="gambar_default" alt="" />
                 </div>
-                <div class="product-thumbs">
+                <div class="product-thumbs" v-if="productDetails.galeries.length>0">
                   <carousel :dots="false" :nav="false" class="product-thumbs-track ps-slider">
-                    <div class="pt" @click="changeImage(thumbs[0])" :class="thumbs[0] == gambar_default ? 'active' : '' ">
-                      <img src="img/mickey1.jpg" alt="" />
-                    </div>
-
-                    <div class="pt" @click="changeImage(thumbs[1])" :class="thumbs[1] == gambar_default ? 'active' : '' ">
-                      <img src="img/mickey2.jpg" alt="" />
-                    </div>
-
-                    <div class="pt" @click="changeImage(thumbs[2])" :class="thumbs[2] == gambar_default ? 'active' : ''">
-                      <img src="img/mickey3.jpg" alt="" />
-                    </div>
-
-                    <div class="pt" @click="changeImage(thumbs[3])" :class="thumbs[3] == gambar_default ? 'active' : ''">
-                      <img src="img/mickey4.jpg" alt="" />
+                    <div v-for="image in productDetails.galeries" :key="image.id"
+                        class="pt" @click="changeImage(image.photo)" :class="image.photo == gambar_default ? 'active' : '' ">
+                      <img :src="image.photo" alt="" />
                     </div>
                   </carousel>
                 </div>
@@ -49,20 +38,12 @@
               <div class="col-lg-6">
                 <div class="product-details text-left">
                   <div class="pd-title">
-                    <span>oranges</span>
-                    <h3>Pure Pineapple</h3>
+                    <span>{{productDetails.type}}</span>
+                    <h3>{{ productDetails.name }}</h3>
                   </div>
                   <div class="pd-desc">
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
-                    </p>
-                    <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
-                      Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
-                      Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
-                      impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
-                    </p>
-                    <h4>$495.00</h4>
+                    <p v-html="productDetails.description"></p>
+                    <h4>${{ productDetails.price }}</h4>
                   </div>
                   <div class="quantity">
                     <router-link to="/cart" class="primary-btn pd-cart">Add To Cart</router-link>
@@ -86,6 +67,7 @@ import HeaderShayna from "@/components/HeaderShayna";
 import FooterShayna from "@/components/FooterShayna";
 import carousel from 'vue-owl-carousel';
 import RelatedShayna from "@/components/RelatedShayna";
+import axios from "axios";
 export default {
   name: 'Product',
   components: {
@@ -98,7 +80,7 @@ export default {
   },
   data(){
     return {
-      gambar_default:"img/mickey1.jpg",
+      gambar_default:"",
         thumbs:[
             "img/mickey1.jpg",
             "img/mickey2.jpg",
@@ -106,13 +88,28 @@ export default {
             "img/mickey4.jpg",
 
         ],
+        productDetails: []
     }
   },
   methods: {
     changeImage(urlImage) {
       this.gambar_default = urlImage;
-    }
+    },
+    setDataPicture(data){
+      this.productDetails = data;
+      this.gambar_default = data.galeries[0].photo;
+    },
   },
+  mounted() {
+    axios
+      .get("http://localhost/shayna-backend/public/api/products",{
+          params: {
+            id: this.$route.params.id
+          }
+      })
+      .then(res => (this.setDataPicture(res.data.data)))
+      .catch(err => console.log(err))
+  }
 }
 </script>
 <style scoped>
