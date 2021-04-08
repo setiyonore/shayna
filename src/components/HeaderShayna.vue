9309b0bd48e87f5657403bbc24566d910031b7c8
setiyonore <setiyono.ressly@gmail.com>
2021-04-08 21:10:13 +0700

    add data from local storage to HeaderShayna.vue

--- src/components/HeaderShayna.vue
+++ src/components/HeaderShayna.vue
@@ -20,7 +20,7 @@
             <div class="col-lg-2 col-md-2">
               <div class="logo">
                 <a href="./index.html">
-                  <img src="img/logo_website_shayna.png" alt="" />
+                  <img  src="img/logo_website_shayna.png" alt="" />
                 </a>
               </div>
             </div>
@@ -31,40 +31,31 @@
                   Keranjang Belanja &nbsp;
                   <a href="#">
                     <i class="icon_bag_alt"></i>
-                    <span>3</span>
+                    <span>{{keranjangUser.length}}</span>
                   </a>
                   <div class="cart-hover">
                     <div class="select-items">
                       <table>
-                        <tbody>
-                        <tr>
+                        <tbody v-if="keranjangUser.length>0">
+                        <tr v-for="keranjang in keranjangUser" :key="keranjang.id">
                           <td class="si-pic">
-                            <img src="img/select-product-1.jpg" alt="" />
+                            <img class="photo-item" :src="keranjang.photo" alt="" />
                           </td>
                           <td class="si-text">
                             <div class="product-selected">
-                              <p>$60.00 x 1</p>
-                              <h6>Kabino Bedside Table</h6>
+                              <p>${{ keranjang.price }}</p>
+                              <h6>{{keranjang.name}}</h6>
                             </div>
                           </td>
                           <td class="si-close">
                             <i class="ti-close"></i>
                           </td>
                         </tr>
-                        <tr>
-                          <td class="si-pic">
-                            <img src="img/select-product-2.jpg" alt="" />
-                          </td>
-                          <td class="si-text">
-                            <div class="product-selected">
-                              <p>$60.00 x 1</p>
-                              <h6>Kabino Bedside Table</h6>
-                            </div>
-                          </td>
-                          <td class="si-close">
-                            <i class="ti-close"></i>
-                          </td>
-                        </tr>
+                        </tbody>
+                        <tbody v-else>
+                          <tr>
+                            <td>Keranjang Kosong</td>
+                          </tr>
                         </tbody>
                       </table>
                     </div>
@@ -90,10 +81,27 @@
 
 <script>
 export default {
-  name: "HeaderShayna"
+  name: "HeaderShayna",
+  data() {
+    return {
+      keranjangUser:[]
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('keranjangUser')) {
+      try {
+        this.keranjangUser = JSON.parse(localStorage.getItem('keranjangUser'));
+      } catch (e) {
+        localStorage.removeItem('keranjangUser');
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.photo-item{
+  width: 80px;
+  height: 80px;
+}
 </style>
