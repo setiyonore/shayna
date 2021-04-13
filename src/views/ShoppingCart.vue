8b20144591f6899cf01f48ed9b84cca0b221e935
setiyonore <setiyono.ressly@gmail.com>
2021-04-13 20:00:33 +0700

    add total harga in ShoppingCart

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -85,7 +85,7 @@
                   <li class="subtotal">ID Transaction <span>#SH12000</span></li>
                   <li class="subtotal mt-3">Subtotal <span>$240.00</span></li>
                   <li class="subtotal mt-3">Pajak <span>10%</span></li>
-                  <li class="subtotal mt-3">Total Biaya <span>$440.00</span></li>
+                  <li class="subtotal mt-3">Total Biaya <span>${{ totalHarga }}.00</span></li>
                   <li class="subtotal mt-3">Bank Transfer <span>Mandiri</span></li>
                   <li class="subtotal mt-3">No. Rekening <span>2208 1996 1403</span></li>
                   <li class="subtotal mt-3">Nama Penerima <span>Shayna</span></li>
@@ -129,7 +129,14 @@ name: "ShoppingCart",
         localStorage.removeItem('keranjangUser');
       }
     }
-  }
+  },
+  computed: {
+    totalHarga() {
+      return this.keranjangUser.reduce(function (items,data){
+        return items + data.price;
+      },0);
+    }
+  },
 }
 </script>
 
